ea5fa1ee3ae1898f2144a23d0ba953342a2956c7
XxX_Vitya_The_Slayer_XxX <vektor910@gmail.com>
2021-12-09 01:52:46 +0300

    complete + extra task

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import json
 
 
+def table_make(stat_param):
+        if round(int(stats[stat_param])/10) > i:
+            return "XXXX"
+        else:
+            return "    "
+
+
 file = open("pokemon_full.json")
 str_pokemon_f = file.read()
 len_str = len(str_pokemon_f)
@@ -13,13 +20,31 @@ for symbol in str_pokemon_f:
         count += 1
 print("Общее количесто символов без пробелов и знаков препинания - ", count)
 
+print("Статы какого покемона вы хотите увидеть?")
+pokemon_src = input()
 objects = json.loads(str_pokemon_f)
 pokemon_name = ""
 abilities = ""
 max_description = 0
 max_words = 0
 for profile in objects:
+    stats = profile["stats"]
     description = profile["description"]
+    if profile["name"] == pokemon_src:
+        for i in reversed(range(0,16)):
+            print("%-8s" % (table_make("hp")), end='')
+            print("%-8s" % (table_make("attack")), end='')
+            print("%-8s" % (table_make("defense")), end='')
+            print("%-8s" % (table_make("sp.atk")), end='')
+            print("%-8s" % (table_make("sp.def")), end='')
+            print("%-8s" % (table_make("speed")))
+            if i == 0:
+                print("%-8s" % ("hp"), end='')
+                print("%-8s" % ("attack"), end='')
+                print("%-8s" % ("defense"), end='')
+                print("%-8s" % ("sp.atk"), end='')
+                print("%-8s" % ("sp.def"), end='')
+                print("%-8s" % ("speed"), "\n")
     for skills in profile["abilities"]:
         if max_words < len(skills.split()):
             max_words = len(skills.split())
