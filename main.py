f9d0fe6fc1285c78f815ed4cab8ebe94c73a6dcd
XxX_Vitya_The_Slayer_XxX <vektor910@gmail.com>
2021-12-09 15:54:10 +0300

    complete + extra task(fixed)

--- main.py
+++ main.py
@@ -29,22 +29,21 @@ max_description = 0
 max_words = 0
 for profile in objects:
     stats = profile["stats"]
+    tags = ["hp", "attack", "defense", "sp.atk", "sp.def", "speed"]
     description = profile["description"]
     if profile["name"] == pokemon_src:
         for i in reversed(range(0,16)):
-            print("%-8s" % (table_make("hp")), end='')
-            print("%-8s" % (table_make("attack")), end='')
-            print("%-8s" % (table_make("defense")), end='')
-            print("%-8s" % (table_make("sp.atk")), end='')
-            print("%-8s" % (table_make("sp.def")), end='')
-            print("%-8s" % (table_make("speed")))
+            for j in range(0,6):
+                if tags[j] != "speed":
+                    print("%-8s" % (table_make(tags[j])), end='')
+                else:
+                    print("%-8s" % (table_make(tags[j])))
             if i == 0:
-                print("%-8s" % ("hp"), end='')
-                print("%-8s" % ("attack"), end='')
-                print("%-8s" % ("defense"), end='')
-                print("%-8s" % ("sp.atk"), end='')
-                print("%-8s" % ("sp.def"), end='')
-                print("%-8s" % ("speed"), "\n")
+                for j in range(0,6):
+                    if tags[j] != "speed":
+                        print("%-8s" % (tags[j]), end='')
+                    else:
+                        print("%-8s" % (tags[j]), "\n")
     for skills in profile["abilities"]:
         if max_words < len(skills.split()):
             max_words = len(skills.split())
